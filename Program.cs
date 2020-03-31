4548d9751234aaf31beadda255019439606bb344
Rickard Marjanovic <rm222jx@student.lnu.se>
2020-03-31 19:15:58 +0200

    Adding all files and creating method

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StatisticsHelper;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 4, 8, 2, 4, 5 };
+
+            Console.WriteLine($"1. Maximum:         : {Statistics.Maximum(test)}");
+            Console.WriteLine($"2. Minimum:         : {Statistics.Minimum(test)}");
+            Console.WriteLine($"3. Medelvärde       : {Statistics.Mean(test)}");
+            Console.WriteLine($"4. Median           : {Statistics.Median(test)}");
+            Console.WriteLine($"5. Typvärde         : {Statistics.Mode(test)}");
+            Console.WriteLine($"6. Variationsbredd  : {Statistics.Range(test)}");
+            Console.WriteLine($"7. Standardavvikelse: {Statistics.StandardDeviation(test)}");
+        }
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace StatisticsHelper {
+    static class Statistics
+    {
+        public static dynamic DescriptiveStatistics(int[] source)
+        {
+            return source;
+        }
+
+        public static int Maximum(int[] source)
+        {
+            int test = 20;
+            return test;
+        }
+
+        public static double Mean(int[] source)
+        {
+            double test = 10.0;
+            return test;
+        }
+
+        public static double Median(int[] source)
+        {
+            double test = 10.0;
+            return test;
+        }
+
+        public static int Minimum(int[] source)
+        {
+            int test = 10;
+            return test;
+        }
+
+        public static int[] Mode(int[] source)
+        {
+            int[] test = new int[] {10, 12};
+            return test;
+        }
+
+        public static int Range(int[] source)
+        {
+            int test = 10;
+            return test;
+        }
+        public static double StandardDeviation(int[] source)
+        {
+            double test = 10.0;
+            return test;
+        }
+    }
+}
