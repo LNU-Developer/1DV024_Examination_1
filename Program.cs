ad47135d74bec7c2de54273e77a623b56c582719
Rickard Marjanovic <rm222jx@student.lnu.se>
2020-04-01 10:39:55 +0200

    Fixing max and min functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using StatisticsHelper;
+using System.Collections.Generic;
 
 namespace Program
 {
@@ -8,14 +9,20 @@ namespace Program
         static void Main(string[] args)
         {
             int[] test = new int[] { 4, 8, 2, 4, 5 };
+            dynamic response = Statistics.DescriptiveStatistics(test);
 
-            Console.WriteLine($"1. Maximum:         : {Statistics.Maximum(test)}");
-            Console.WriteLine($"2. Minimum:         : {Statistics.Minimum(test)}");
-            Console.WriteLine($"3. Medelvärde       : {Statistics.Mean(test)}");
-            Console.WriteLine($"4. Median           : {Statistics.Median(test)}");
-            Console.WriteLine($"5. Typvärde         : {Statistics.Mode(test)}");
-            Console.WriteLine($"6. Variationsbredd  : {Statistics.Range(test)}");
-            Console.WriteLine($"7. Standardavvikelse: {Statistics.StandardDeviation(test)}");
+            foreach (KeyValuePair<string, dynamic> item in response)
+            {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            //Console.WriteLine($"1. Maximum:         : {response.Maximum}");
+            //Console.WriteLine($"2. Minimum:         : {response.Minimum}");
+            //Console.WriteLine($"3. Medelvärde       : {response.Mean}");
+            //Console.WriteLine($"4. Median           : {response.Median}");
+            //Console.WriteLine($"5. Typvärde         : {response.Mode}");
+            //Console.WriteLine($"6. Variationsbredd  : {response.Range}");
+            //Console.WriteLine($"7. Standardavvikelse: {response.StandardDeviation}");
         }
     }
 }
--- Statistics.cs
+++ Statistics.cs
@@ -1,52 +1,80 @@
 using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace StatisticsHelper {
     static class Statistics
     {
         public static dynamic DescriptiveStatistics(int[] source)
         {
+            
+            Dictionary<string, dynamic> response =   
+                       new Dictionary<string, dynamic>()
+                       {
+                           {"Maximum", Maximum(source)},
+                           {"Minimum", Minimum(source)},
+                           {"Mean", Mean(source)}, 
+                           {"Median", Median(source)},
+                           {"Mode", Mode(source)},
+                           {"Range", Range(source)},
+                           {"Standard Deviation", StandardDeviation(source)}
+                       };
+        
+            return response;
+        }
+
+        static int Maximum(int[] source)
+        {
+            int[] sortedArray = uniqueSortedArray(source);
+            return sortedArray[sortedArray.Length-1];
+        }
+
+        static double Mean(int[] source)
+        {
+            double test = 10.0;
+            return test;
+        }
+
+        static double Median(int[] source)
+        {
+            double test = 10.0;
+            return test;
+        }
+
+        static int Minimum(int[] source)
+        {
+            int[] sortedArray = uniqueSortedArray(source);
+            return sortedArray[0];
+        }
+
+        static int[] Mode(int[] source)
+        {
             return source;
         }
 
-        public static int Maximum(int[] source)
-        {
-            int test = 20;
-            return test;
-        }
-
-        public static double Mean(int[] source)
-        {
-            double test = 10.0;
-            return test;
-        }
-
-        public static double Median(int[] source)
-        {
-            double test = 10.0;
-            return test;
-        }
-
-        public static int Minimum(int[] source)
+        static int Range(int[] source)
         {
             int test = 10;
             return test;
         }
-
-        public static int[] Mode(int[] source)
-        {
-            int[] test = new int[] {10, 12};
-            return test;
-        }
-
-        public static int Range(int[] source)
-        {
-            int test = 10;
-            return test;
-        }
-        public static double StandardDeviation(int[] source)
+        static double StandardDeviation(int[] source)
         {
             double test = 10.0;
             return test;
+        }
+
+        static int[] uniqueSortedArray (int[] source) {
+            
+            int[] deepCopy = new int[source.Length];
+
+            for (int i=0; i<source.Length; i++) // Iterate through array, one element at a time
+            {
+            deepCopy[i] = source[i];
+            }
+ 
+            
+            Array.Sort(deepCopy);
+            return deepCopy;
         }
     }
 }
